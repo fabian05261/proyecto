6e3fec27957d19e89cc18d7393e2f818662de5a2
Lijuroe <julianparojas@gmail.com>
2020-09-21 12:14:49 -0500

    Cliente

---
+++ proyecto/src/main/java/entity/Cliente.java
@@ -0,0 +1,73 @@
+package Entity;
+
+public class Cliente {
+  private long codigoCliente;
+  private long cedula;
+  private String nombre;
+  private String Apellidos;
+  private String direccionEntrega;
+  private long telefono;
+
+    public Cliente(long codigoCliente, long cedula, String nombre, String Apellidos, String direccionEntrega, long telefono) {
+        this.codigoCliente = codigoCliente;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.Apellidos = Apellidos;
+        this.direccionEntrega = direccionEntrega;
+        this.telefono = telefono;
+    }
+    
+    public long getCodigoCliente() {
+        return codigoCliente;
+    }
+
+    public void setCodigoCliente(long codigoCliente) {
+        this.codigoCliente = codigoCliente;
+    }
+
+    public long getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(long cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getDireccionEntrega() {
+        return direccionEntrega;
+    }
+
+    public void setDireccionEntrega(String direccionEntrega) {
+        this.direccionEntrega = direccionEntrega;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "codigoCliente=" + codigoCliente + ", cedula=" + cedula + ", nombre=" + nombre + ", Apellidos=" + Apellidos + ", direccionEntrega=" + direccionEntrega + ", telefono=" + telefono + '}';
+    }
+       
+}
