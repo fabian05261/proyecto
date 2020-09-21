e22f33f2353530359b9de09c93c93747b53a5d10
fabianzboni <fabianzapata2001@hotmail.com>
2020-09-21 13:05:07 -0500

    gestion clientes

---
+++ proyecto/src/main/java/control/GestionClientes.java
@@ -0,0 +1,42 @@
+package control;
+
+import java.util.ArrayList;
+
+import entity.*;
+
+public class GestionClientes {
+	
+	private ArrayList<Cliente>cliente=new ArrayList<Cliente>();
+
+	public ArrayList<Cliente> getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(ArrayList<Cliente> cliente) {
+		this.cliente = cliente;
+	}
+	
+	public void VerClientes(Cliente cliente1)
+	{
+		for(Cliente clientes:cliente)
+		{
+			if(cliente1.getCedula()==clientes.getCedula())
+			{
+				System.out.println(cliente1.getCedula());
+			}
+		}
+	}
+	public void InsertarCliente(Cliente cliente1)
+	{
+		for(Cliente clientes:cliente)
+		{
+			if(cliente1.getCedula()==clientes.getCedula())
+			{
+				System.out.println("El cliente esta repetido");
+				return;
+			}
+			System.out.println("Se inserto el cliente");
+			cliente.add(cliente1);
+		}
+	}
+}
--- proyecto/src/main/java/entity/Cliente.java
+++ proyecto/src/main/java/entity/Cliente.java
@@ -1,4 +1,4 @@
-package Entity;
+package entity;
 
 public class Cliente {
   private long codigoCliente;
