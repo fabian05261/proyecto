6347f5d90fc1a4fb1b883534048cbd75d5306f3e
bustacar03 <bustacararjuan@gmail.com>
2020-09-22 20:07:20 -0500

    update de compra

--- proyecto/src/main/java/entity/Compra.java
+++ proyecto/src/main/java/entity/Compra.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package entity;
+import java.util.Calendar;
 
 /**
  *
  * @author Sebastián Bustacara
  */
 public class Compra {
+    private long codigoCompra;
+    private Calendar fecha;
+    private boolean pagado;
     
+    Compra(){};
+    
+    Compra(long codigoCompra, Calendar fecha, boolean pagado){
+        this.codigoCompra=codigoCompra;
+        this.fecha=fecha;
+        this.pagado=pagado;
+    }
 }
