d1e88eda5e4cfd03c06ddfc392376d1cc605e4dd
fabianzboni <fabianzapata2001@hotmail.com>
2020-09-22 20:50:40 -0500

    declaracion control galeria

--- proyecto/src/main/java/control/ControlGaleria.java
+++ proyecto/src/main/java/control/ControlGaleria.java
@@ -1,6 +1,40 @@
 package control;
+import java.util.*;
+import entity.*;
 
 public class ControlGaleria {
 
+	private ArrayList<Cliente>listaClientes=new ArrayList <Cliente>();
+	private ArrayList<Artista>listaArtistas=new ArrayList <Artista>();
+	private ArrayList<Obras>listaObras=new ArrayList <Obras>();
+
+	public ControlGaleria()
+	{
+		
+	}
+	public ArrayList<Cliente> getListaClientes() {
+		return listaClientes;
+	}
+
+	public void setListaClientes(ArrayList<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
+	public ArrayList<Artista> getListaArtistas() {
+		return listaArtistas;
+	}
+
+	public void setListaArtistas(ArrayList<Artista> listaArtistas) {
+		this.listaArtistas = listaArtistas;
+	}
+
+	public ArrayList<Obras> getListaObras() {
+		return listaObras;
+	}
+
+	public void setListaObras(ArrayList<Obras> listaObras) {
+		this.listaObras = listaObras;
+	}
+	
 	
 }
