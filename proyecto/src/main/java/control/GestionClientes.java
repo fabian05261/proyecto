10e171e82cc2a1e693712b54aef7068e96b59e2b
Lijuroe <julianparojas@gmail.com>
2020-09-26 13:28:29 -0500

    Update GestionClientes.java

--- proyecto/src/main/java/control/GestionClientes.java
+++ proyecto/src/main/java/control/GestionClientes.java
@@ -6,33 +6,45 @@ import java.util.Scanner;
 
 //crea e instancias los Clientes y llenando la lista de clientes de la clase ControlGaleria
 
-public class GestionClientes(ArrayList<Cliente>listaClientes){
-    
-    int i = 0;
+public class GestionClientes{
+   
+    public void inListaClientes(ArrayList<Cliente> listaClientes){
+        int clie = 0;
 
-    Cliente 1 = new Cliente(12345,4052,"Julian","Rojas","Crr 6 N4",3124351236);
-    Cliente 2 = new Cliente(12346,4053,"Juan","González Flores","Crr 1 N3",3205264789);
-    Cliente 3 = new Cliente(12347,4054,"Pedro","González Ramírez","Cll 2 N11",3104556789);
-    Cliente 4 = new Cliente(12348,4055,"Mario","Gómez Ruiz","Crr 2 N6",3135294436);
-    Cliente 5 = new Cliente(12349,4056,"Maria","Fernández","Crr 4 N9",3322561245);
-    Cliente 6 = new Cliente(12350,4057,"Andres","López","Cll 8 N2",3103104547);
-    Cliente 7 = new Cliente(12351,4058,"Pablo","Díaz Martínez","Diag 3 N13",300212363);
-    Cliente 8 = new Cliente(12352,4059,"Diego","Pérez","Crr 9 N4",3057465248);
-    Cliente 9 = new Cliente(12353,4060,"Felipe","García Sánchez","Diag 6 N7",3113107821);
-    Cliente 10 = new Cliente(12354,4061,"Andrea","Romero","Crr 8 N1",3234241562);
-    Cliente 11 = new Cliente(12355,4062,"Laura","Sosa Torres","Cll 9 N8",3123254661);
-    Cliente 12 = new Cliente(12356,4063,"Liliana","Álvarez","Diag 6 N5",3005299037);
+        Cliente julian = new Cliente(12345,4052,"Julian","Rojas","Crr 6 N4",31243512);
+        listaClientes.add(julian);
+        Cliente juan = new Cliente(12346,4053,"Juan","González Flores","Crr 1 N3",32052647);
+        listaClientes.add(juan);
+        Cliente pedro = new Cliente(12347,4054,"Pedro","González Ramírez","Cll 2 N11",31045567);
+        listaClientes.add(pedro);
+        Cliente mario = new Cliente(12348,4055,"Mario","Gómez Ruiz","Crr 2 N6",31352944);
+        listaClientes.add(mario);
+        Cliente maria = new Cliente(12349,4056,"Maria","Fernández","Crr 4 N9",33225612);
+        listaClientes.add(maria);
+        Cliente andres = new Cliente(12350,4057,"Andres","López","Cll 8 N2",31031045);
+        listaClientes.add(andres);
+        Cliente pablo = new Cliente(12351,4058,"Pablo","Díaz Martínez","Diag 3 N13",3002123);
+        listaClientes.add(pablo);
+        Cliente diego = new Cliente(12352,4059,"Diego","Pérez","Crr 9 N4",30574652);
+        listaClientes.add(diego);
+        Cliente felipe = new Cliente(12353,4060,"Felipe","García Sánchez","Diag 6 N7",31131078);
+        listaClientes.add(felipe);
+        Cliente andrea = new Cliente(12354,4061,"Andrea","Romero","Crr 8 N1",32342415);
+        listaClientes.add(andrea);
+        Cliente laura = new Cliente(12355,4062,"Laura","Sosa Torres","Cll 9 N8",31232546);
+        listaClientes.add(laura);
+        Cliente liliana = new Cliente(12356,4063,"Liliana","Álvarez","Diag 6 N5",30052990);
+        listaClientes.add(liliana);
 
-    for(int j=1 ; j<13 ; j++){
-        listaClientes.add(j);
+        System.out.println("\n Se ingresaron los siguientes clientes:");
+        //Reviso si la lista realmente se lleno
+
+        for(Cliente aux : listaClientes){
+            clie++;
+            System.out.println(aux.toString());
+        }
+        System.out.println("Se anadieron a la lista de clientes" + clie + "nuevos clientes");
     }
-    System.out.println("\n Se ingresaron los siguientes clientes:");
-    //Reviso si la lista realmente se lleno;
-    for(Cliente aux : listaClientes){
-        i++;
-        System.out.println(aux.toString());
-    }
-    system.out.println("\n Se inicializaron %i clientes"+i);
 }
 
 /* public void listadodeClientes(){
